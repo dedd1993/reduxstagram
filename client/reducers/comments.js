9cf1964dc86bce870555f56849a71f0ff69d248f
Daniel <dedd1993@gmail.com>
2019-07-31 16:57:03 -0500

    updating comment state in our store

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,31 @@
+function postComments(state = [], action) {
+    switch(action.type) {
+        case 'ADD_COMMENT':
+            // return the new instance with the new comment
+            return [...state, {
+                user: action.author,
+                text: action.comment,
+            }];
+        case 'REMOVE_COMMENT':
+            // we need to return the new state without the deleted comment
+            return [
+                ...state.filter((comment, index) => index !== action.i )
+            ];
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments(state = [], action) {
+    if (typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        };
+    }
     return state;
 }
 
